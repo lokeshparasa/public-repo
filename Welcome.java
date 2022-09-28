889d0e885503779cb661c7f07882bf49bcd26c01
lokeshparasa <114305997+lokeshparasa@users.noreply.github.com>
2022-09-28 22:12:26 +0530

    first commit

---
+++ Welcome.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+
+public class Welcome {
+
+public static void main(String[] args) throws Exception {
+int a=10,b=20;
+System.out.println("Welcome to java");
+System.out.println(a+b);
+}
+}
